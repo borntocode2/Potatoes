d67f19a6cf373d5060db70272d575801a8f71fe1
ralph <sanghwaj222@gmail.com>
2024-08-23 12:35:18 +0900

    0823

--- hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,6 +3,7 @@ import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -16,7 +17,30 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model){ //브라우저에서 ?name = spring 같이 파라미터넘김
         model.addAttribute("name", name); //model을 받고, model에다가 scanf처럼 변수를 넣어주었따.
         return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody//HTTP의 BODY에 문자내용을 직접 반환
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
 
+    @GetMapping("hello-api")
+    @ResponseBody //hello-api가 매핑되어있네? 어라 ?ResponseBody네?
+                  // hello 객체에 name을 파라미터로 넣은 상태로 반환
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
 
+        public String getName(){
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
     }
 }
---
+++ hello-spring/src/main/java/hello/hello_spring/domain/Member.java
@@ -0,0 +1,20 @@
+package hello.hello_spring.domain;
+
+public class Member {
+
+    private Long id;//데이터를 구분하기위해 시스템이정하는 id
+    private String name;
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+}
